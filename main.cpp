139e5d952c1f15931a3340487ed8b8af3890adf0
Sahil Patel <spate125@ucr.edu>
2019-08-04 00:56:23 +0100

    Add user input

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
 int main(){
-	std::cout << "hello git" << std::endl;
-	return 1;
-}
+
+	std::string name;
+	std::cout << "What is your name?" << std::end
+	std::cin >> name;
+	std::cout << "Hello " << name << "!" << std::endl;
+	
+	return 1:
+	
+	
 
